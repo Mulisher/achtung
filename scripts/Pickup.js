7dbac6c51f5694ad6d61294c918ee10eb5cc984b
Mulisher <mechanik0@gmail.com>
2024-02-07 10:33:47 +0100

    initial commit

---
+++ scripts/Pickup.js
@@ -0,0 +1,657 @@
+//TODO
+//2 rozne randomy?
+//wysyp znajdziek?
+//chyba poparawki na romzar itp powinny być wewnatrz
+//"first tick"
+const pickupTypes = [ 'speed_self',
+                      'speed_others',
+                      'slow_self',
+                      'slow_others',
+
+                      'fat_self',
+                      'fat_others',
+                      'thin_self',
+                      'thin_others',
+
+                      'clear_all_trails',
+                      'clear_my_trail',
+                      'tron_self',
+                      'tron_others',
+
+                      'no_walls',
+                      'wall_hack',
+                      'ghost',
+                      'random']
+class Pickup{
+  constructor(){
+    this.id = this.generateId()
+    const sx = w/50+random(gameBorder-(w/50)*2)
+    const sy = w/50+random(h-(w/50)*2)
+    this.position = createVector(sx,sy)
+    const rand = Math.floor(random(pickupTypes.length))
+    this.type = pickupTypes[rand]
+    //this.type = 'thin_self'
+    this.r = w/50
+    this.picked = false
+    this.firstTick = true
+    this.pickedById = -1
+    this.timeLeft = this.provideDuration()
+    this.affected = []
+  }
+
+  update(){
+    if(this.checkIfPickedUp()){
+      this.picked = true
+    }
+    if(!this.picked)return
+    switch (this.type){
+      case 'speed_self':
+        this.speedSelf()
+        break
+      case 'speed_others':
+        this.speedOthers()
+        break
+      case 'slow_self':
+        this.slowSelf()
+        break
+      case 'slow_others':
+        this.slowOthers()
+        break
+      case 'fat_self':
+        this.fatSelf()
+        break
+      case 'fat_others':
+        this.fatOthers()
+        break
+      case 'thin_self':
+        this.thinSelf()
+        break
+      case 'thin_others':
+        this.thinOthers()
+        break
+      case 'tron_self':
+        this.tronSelf()
+        break
+      case 'tron_others':
+        this.tronOthers()
+        break
+      case 'clear_all_trails':
+        this.clearAllTrails()
+        break
+      case 'clear_my_trail':
+        this.clearMyTrail()
+        break
+      case 'ghost':
+        this.ghost()
+        break
+      case 'no_walls':
+        this.noWalls()
+        break
+      case 'wall_hack':
+        this.wallHack()
+        break
+      case 'random':
+        this.random()
+        break
+      default:
+    }
+  }
+
+  getWhoPicked(){
+    for(let p of players){
+      if(p.id == this.pickedById){
+        return p
+      }
+    }
+  }
+
+  speedSelf(){
+    if(this.timeLeft > 0){
+      if(this.firstTick){
+        let pl = this.getWhoPicked()
+        pl.speed *= 2
+        this.firstTick = false
+      }
+      this.timeLeft--
+    }else{
+      let pl = this.getWhoPicked()
+      pl.speed /= 2
+      this.remove()
+    }
+  }
+
+  speedOthers(){
+    if(this.timeLeft > 0){
+      if(this.firstTick){
+        this.affected = []
+        this.firstTick = false
+        for(let p of players){
+          if(p.id != this.pickedById){
+            this.affected.push(p.id)
+            p.speed *= 2
+          }
+        }
+      }
+      this.timeLeft--
+    }else{
+      for(let p of players){
+        if(this.affected.includes(p.id)){
+          p.speed /= 2
+        }
+      }
+      this.remove()
+    }
+  }
+
+  slowSelf(){
+    if(this.timeLeft > 0){
+      if(this.firstTick){
+        let p = this.getWhoPicked()
+        p.speed /= 2
+        this.firstTick = false
+      }
+      this.timeLeft--
+    }else{
+      let p = this.getWhoPicked()
+      p.speed *= 2
+      this.remove()
+    }
+  }
+
+  slowOthers(){
+    if(this.timeLeft > 0){
+      if(this.firstTick){
+        this.affected = []
+        this.firstTick = false
+        for(let p of players){
+          if(p.id != this.pickedById){
+            this.affected.push(p.id)
+            p.speed /= 2
+          }
+        }
+      }
+      this.timeLeft--
+    }else{
+      for(let p of players){
+        if(this.affected.includes(p.id)){
+          p.speed *= 2
+        }
+      }
+      this.remove()
+    }
+  }
+
+  fatSelf(){
+    let pl = this.getWhoPicked()
+    if(this.timeLeft > 0){
+      if(this.firstTick){
+        pl.interuptTrail()
+        pl.size *= 2
+        pl.holeSize *= 1.75
+        this.firstTick = false
+      }
+      if(pl.size > baseSize*4){
+        pl.size /= 2
+        pl.holeSize /= 1.75
+        this.remove()
+      }
+      this.timeLeft--
+    }else{
+      pl.interuptTrail()
+      pl.size /= 2
+      pl.holeSize /= 1.75
+      this.remove()
+    }
+  }
+
+  fatOthers(){
+    if(this.timeLeft > 0){
+      if(this.firstTick){
+        this.affected = []
+        this.firstTick = false
+        for(let p of players){
+          if(p.id != this.pickedById){
+            this.affected.push(p.id)
+            p.interuptTrail()
+            p.size *= 2
+            p.holeSize *= 1.75
+          }
+        }
+      }
+      for(let p of players){
+        if(p.size > baseSize*4){
+          p.size /= 2
+          p.holeSize /= 1.75
+          this.removeAffected(p.id)
+        }
+      }
+      this.timeLeft--
+    }else{
+      for(let p of players){
+        if(this.affected.includes(p.id)){
+          p.interuptTrail()
+          p.size /= 2
+          p.holeSize /= 1.75
+        }
+      }
+      this.remove()
+    }
+  }
+
+  thinSelf(){
+    let pl = this.getWhoPicked()
+    if(this.timeLeft > 0){
+      if(this.firstTick){
+        pl.interuptTrail()
+        pl.size /= 2
+        pl.holeSize /= 1.25
+        this.firstTick = false
+      }
+      if(pl.size < baseSize/4){
+        pl.size *= 2
+        pl.holeSize *= 1.25
+        this.remove()
+      }
+      this.timeLeft--
+    }else{
+      pl.interuptTrail()
+      pl.size *= 2
+      pl.holeSize *= 1.25
+      this.remove()
+    }
+  }
+
+  thinOthers(){
+    if(this.timeLeft > 0){
+      if(this.firstTick){
+        this.affected = []
+        this.firstTick = false
+        for(let p of players){
+          if(p.id != this.pickedById){
+            this.affected.push(p.id)
+            p.interuptTrail()
+            p.size /= 2
+            p.holeSize /= 1.25
+          }
+        }
+      }
+      for(let p of players){
+        if(p.size < baseSize/2){
+          p.size *= 2
+          p.holeSize *= 1.25
+          this.removeAffected(pId)
+        }
+      }
+      this.timeLeft--
+    }else{
+      for(let p of players){
+        if(this.affected.includes(p.id)){
+          p.interuptTrail()
+          p.size *= 2
+          p.holeSize *= 1.25
+        }
+      }
+      this.remove()
+    }
+  }
+
+  clearAllTrails(){
+    for(let p of players){
+      p.history = []
+    }
+    this.remove()
+  }
+
+  clearMyTrail(){
+    let p = this.getWhoPicked()
+    p.history = []
+    this.remove()
+  }
+
+  tronSelf(){
+    let pl = this.getWhoPicked()
+    if(this.timeLeft > 0){
+      pl.tron = true
+      this.timeLeft--
+    }else{
+      let otherTron = false
+      for(let p of pickups){
+        if(p.picked &&
+           p.id != this.id &&
+           p.type == this.type &&
+           p.pickedById == this.pickedById){
+          otherTron = true
+        }
+      }
+      if(!otherTron){
+        pl.tron = false
+      }
+      this.remove()
+    }
+  }
+
+  tronOthers(){
+    if(this.timeLeft > 0){
+      if(this.firstTick){
+        this.affected = []
+        this.firstTick = false
+        for(let p of players){
+          if(p.id != this.pickedById){
+            this.affected.push(p.id)
+            p.tron = true
+          }
+        }
+      }
+      for(let p of players){
+        if(p.id != this.pickedById){
+          this.affected.push(p.id)
+          p.tron = true
+        }
+      }
+      this.timeLeft--
+    }else{
+      for(let p of players){
+        if(this.affected.includes(p.id)){
+          p.tron = false
+        }
+      }
+      this.remove()
+    }
+  }
+
+  ghost(){
+    let pl = this.getWhoPicked()
+    if(this.timeLeft > 0){
+      pl.ghost = true
+      this.timeLeft--
+    }else{
+      let otherGhost = false
+      for(let p of pickups){
+        if(p.picked &&
+           p.id != this.id &&
+           p.type == this.type &&
+           p.pickedById == this.pickedById){
+           otherGhost = true
+        }
+      }
+      if(!otherGhost){
+        pl.ghost = false
+      }
+      this.remove()
+    }
+  }
+
+  noWalls(){
+    if(this.timeLeft > 0){
+      walls = false
+      this.timeLeft--
+    }else{
+      let otherNoWalls = false
+      for(let p of pickups){
+        if(p.picked && p.id != this.id && p.type == this.type){
+          otherNoWalls = true
+        }
+      }
+      if(!otherNoWalls){
+        walls = true;
+      }
+      this.remove()
+    }
+  }
+
+  wallHack(){
+    let pl = this.getWhoPicked()
+    if(this.timeLeft > 0){
+      pl.wallHack = true
+      this.timeLeft--
+    }else{
+      let otherWallHacks = false
+      for(let p of pickups){
+        if(p.picked &&
+           p.id != this.id &&
+           p.type == this.type &&
+           p.pickedById == this.pickedById){
+          otherWallHacks = true
+        }
+      }
+      if(!otherWallHacks){
+        pl.wallHack = false
+      }
+      this.remove()
+    }
+  }
+
+  random(){
+    const rand = Math.floor(random(pickupTypes.length))
+    this.type = pickupTypes[rand]
+    this.update()
+  }
+
+  getAffected(aId){
+    for(let p of players){
+      if(aId == p.id)return p
+    }
+  }
+
+  removeAffected(pId){
+    for(let i = this.affected.length - 1; i >= 0; i--){
+      if(this.affected[i] == pId){
+        this.affected.splice(i,1)
+      }
+    }
+  }
+
+  display(){
+    if(this.picked)return
+    const pos = this.position
+    switch (this.type){
+      case 'speed_self':
+        this.ShowSpeedSelf(pos)
+        break
+      case 'speed_others':
+        this.ShowSpeedOthers(pos)
+        break
+      case 'slow_self':
+        this.ShowSlowSelf(pos)
+        break
+      case 'slow_others':
+        this.ShowSlowOthers(pos)
+        break
+      case 'fat_self':
+        this.ShowFatSelf(pos)
+        break
+      case 'fat_others':
+        this.ShowFatOthers(pos)
+        break
+      case 'thin_self':
+        this.ShowThinSelf(pos)
+        break
+      case 'thin_others':
+        this.ShowThinOthers(pos)
+        break
+      case 'tron_self':
+        this.ShowTronSelf(pos)
+        break
+      case 'tron_others':
+        this.ShowTronOthers(pos)
+        break
+      case 'clear_all_trails':
+        this.ShowClearAllTrails(pos)
+        break
+      case 'clear_my_trail':
+        this.ShowClearMyTrail(pos)
+        break
+      case 'ghost':
+        this.ShowGhost(pos)
+        break
+      case 'no_walls':
+        this.ShowNoWalls(pos)
+        break
+      case 'wall_hack':
+        this.ShowWallHack(pos)
+        break
+      case 'random':
+        this.ShowRandom(pos)
+        break
+      default:
+    }
+  }
+
+  ShowSpeedSelf(pos){
+    this.textIconSelf(pos,'\u2b4d')
+  }
+  ShowSpeedOthers(pos){
+    this.textIconOther(pos,'\u2b4d')
+  }
+  ShowSlowSelf(pos){
+    this.textIconSelf(pos,'\u2a54')
+  }
+  ShowSlowOthers(pos){
+    this.textIconOther(pos,'\u2a54')
+  }
+  ShowFatSelf(pos){
+    this.textIconSelf(pos,'\u2501')
+  }
+  ShowFatOthers(pos){
+    this.textIconOther(pos,'\u2501')
+  }
+  ShowThinSelf(pos){
+    this.textIconSelf(pos,'\u23bb')
+  }
+  ShowThinOthers(pos){
+    this.textIconOther(pos,'\u23bb')
+  }
+  ShowTronSelf(pos){
+    this.textIconSelf(pos,'\u2ba3')
+  }
+  ShowTronOthers(pos){
+    this.textIconOther(pos,'\u2ba3')
+  }
+  ShowClearAllTrails(pos){
+    this.textIcon(pos,'\u2505')
+  }
+  ShowClearMyTrail(pos){
+    this.textIconSelf(pos,'\u2505')
+  }
+  ShowGhost(pos){
+    this.textIconSelf(pos,'\u233e')
+  }
+  ShowNoWalls(pos){
+    this.textIcon(pos,'\u26f6')
+  }
+  ShowWallHack(pos){
+    this.textIconSelf(pos,'\u2346')
+  }
+  ShowRandom(pos){
+    this.textIcon(pos,'?')
+  }
+
+  textIcon(pos,txt){
+    push()
+    let ofs = w/500
+    translate(pos.x,pos.y)
+    noStroke()
+    fill(lColors[1])
+    ellipse(0,-ofs,this.r,this.r)
+    textSize(w/50)
+    fill(color(0xfb,0xf1,0xc7))
+    text(txt,0,0)
+    pop()
+  }
+
+  textIconSelf(pos,txt){
+    push()
+    let ofs = w/500
+    translate(pos.x,pos.y)
+    noStroke()
+    fill(lColors[2])
+    ellipse(0,-ofs,this.r,this.r)
+    textSize(w/50)
+    fill(color(0xfb,0xf1,0xc7))
+    text(txt,0,0)
+    pop()
+  }
+
+  textIconOther(pos,txt){
+    push()
+    let ofs = w/500
+    translate(pos.x,pos.y)
+    noStroke()
+    fill(lColors[0])
+    ellipse(0,-ofs,this.r,this.r)
+    textSize(w/50)
+    fill(color(0xfb,0xf1,0xc7))
+    text(txt,0,0)
+    pop()
+  }
+
+  generateId(){
+    let exsistingIds = []
+    for(let p of pickups){
+      exsistingIds.push(p.id)
+    }
+    let newId = Math.floor(random(100000))
+    while(exsistingIds.includes(newId)){
+      newId = Math.floor(random(100000))
+    }
+    return newId
+  }
+
+  remove(){
+    this.toBeRemoved = true
+  }
+
+  checkIfPickedUp(){
+    if(this.picked)return false
+    const pos = this.position
+    let min = 9999
+    let whoIsClosest
+    for(let p of players){
+      const pl = p.position
+      const d = dist(pl.x,pl.y,pos.x,pos.y)
+      if(d < min){
+        min = d
+        whoIsClosest = p.id
+      }
+    }
+    if(min < this.r){
+      this.pickedById = whoIsClosest
+      return true
+    }
+    return false
+  }
+
+  provideDuration(){
+    switch (this.type){
+      case 'speed_self':
+        return 30*4
+      case 'speed_others':
+        return 30*4
+      case 'slow_self':
+        return 30*4
+      case 'slow_others':
+        return 30*4
+      case 'fat_self':
+        return 30*8
+      case 'fat_others':
+        return 30*8
+      case 'thin_self':
+        return 30*8
+      case 'thin_others':
+        return 30*8
+      case 'tron_self':
+        return 30*8
+      case 'tron_others':
+        return 30*8
+      case 'ghost':
+        return 30*8
+      case 'no_walls':
+        return 30*10
+      case 'wall_hack':
+        return 30*10
+      default:
+        return 0
+    }
+  }
+
+}
---
+++ scripts/Player.js
@@ -0,0 +1,200 @@
+class Player{
+  constructor(id){
+    this.id = id
+    this.color = pColors[id]
+    this.score = 0
+    this.resetForStart()
+  }
+
+  resetForStart(){
+    const sx = w/50+random(gameBorder-(w/50)*2)
+    const sy = w/50+random(h-(w/50)*2)
+    this.position = createVector(sx,sy)
+    this.velocity = createVector(1,1)
+    this.velocity.setMag(baseSpeed)
+    this.velocity.rotate( radians( random(360) ) )
+    this.speed = 5
+    this.size = baseSize
+    this.history = []
+    this.recentHole = 240
+    this.holeSize = 26
+    this.makingHole = 0
+    this.alive = true
+    this.wallHack = false
+    this.ghost = false
+    this.tron = false
+    this.lLock = false
+    this.rLock = false
+  }
+
+  update(){
+    if(!this.alive)return
+
+    if(this.tron){
+      const rot = radians(90)
+      if(controls[this.id][0] && !this.lLock){
+        this.velocity.rotate(-rot)
+        this.lLock = true
+      }
+      if(controls[this.id][1] && !this.rLock){
+        this.velocity.rotate(rot)
+        this.rLock = true
+      }
+      if(!controls[this.id][0])this.lLock = false
+      if(!controls[this.id][1])this.rLock = false
+    }
+    else{
+      if(controls[this.id][0]){this.velocity.rotate(-rotS)}
+      if(controls[this.id][1]){this.velocity.rotate(rotS)}
+    }
+
+    for(let i = 0; i < this.speed; i++){
+      const pos = this.position.copy()
+      if(this.makingHole > 0){
+        this.history.push([pos,true,this.size])
+        this.makingHole --
+      }
+      else if( random(100) < 1 && this.recentHole < 0 ||
+               this.ghost ){
+        this.history.push([pos,true,this.size])
+        this.recentHole = 240
+        this.makingHole = this.holeSize
+      }
+      else{
+        this.history.push([pos,false,this.size])
+        this.recentHole --
+      }
+      this.position.add(this.velocity)
+    }
+  }
+
+  display(){
+    if(pause){
+      push()
+      translate(this.position.x,this.position.y)
+      stroke(fgC)
+      strokeWeight(2)
+      let sl = this.velocity.copy().setMag(20)
+      line(0,0,sl.x,sl.y)
+      pop()
+    }
+    this.drawTrail()
+    strokeWeight(1)
+    if(this.wallHack){
+      fill  (fgC)
+      stroke(fgC)
+    }else{
+      fill  (this.color)
+      stroke(this.color)
+    }
+    if(this.tron){
+      push()
+      rectMode(CENTER)
+      translate(this.position.x,this.position.y)
+      rotate(this.velocity.heading())
+      rect(0,0,this.size*2,this.size*2)
+      pop()
+    }else{
+      ellipse(this.position.x,this.position.y,this.size*2,this.size*2)
+    }
+  }
+
+  drawTrail(){
+    stroke(this.color)
+    noFill()
+    beginShape()
+    for(let i = 0; i < this.history.length; i++){
+      strokeWeight(this.history[i][2]*2)
+      let hp = this.history[i][0];
+      if(this.history[i][1] == true){
+        endShape()
+        beginShape()
+      }else{
+        vertex(hp.x,hp.y)
+      }
+    }
+    endShape()
+  }
+
+  checkCollision(){
+    const pos = this.position
+    const bT = borderThickness/2
+    const s = this.size
+    let xr = pos.x + s > gameBorder - bT
+    let xl = pos.x - s < bT
+    let yd = pos.y + s > height - bT
+    let yu = pos.y - s < bT
+
+    if(xr | xl | yd | yu){
+      if(walls && !this.wallHack && !this.ghost){
+        this.crash()
+      }
+      else{
+        this.interuptTrail()
+        if(xr)     { this.position.x = 1+s+bT }
+        else if(xl){ this.position.x = gameBorder-bT-1 }
+        if(yd)     { this.position.y = 1+s+bT }
+        else if(yu){ this.position.y = height-s-bT-1 }
+        this.interuptTrail()
+      }
+    }
+
+    if(this.ghost)return
+    if(this.checkSelfHit()){
+      this.crash()
+      return
+    }
+
+    for(let p of players){
+      if(p.id != this.id && p.checkOtherHit(pos,s)){
+        this.crash()
+        return
+      }
+    }
+  }
+
+  checkOtherHit(pos,siz){
+    for(let i = 0; i < this.history.length; i++){
+      let tp = this.history[i][0]
+      let hole = this.history[i][1]
+      let d = siz + this.history[i][2]
+      let test = dist(pos.x,pos.y, tp.x,tp.y) < d
+      if(!hole & test){
+        return true
+      }
+    }
+    return false
+  }
+
+  checkSelfHit(){
+    let skip = this.size * 8
+    for(let i = 0; i < this.history.length - skip; i++){
+      let tp = this.history[i][0]
+      let pos = this.position
+      let hole = this.history[i][1]
+      let d = this.size + this.history[i][2]
+      let test = dist(pos.x,pos.y, tp.x,tp.y) < d
+      if(!hole & test){
+        return true
+      }
+    }
+    return false
+  }
+
+  interuptTrail(){
+    let pos = this.position
+    this.history.push([pos,true,this.size])
+  }
+
+  crash(){
+    this.alive = false
+    this.giveScore()
+    checkRoundWin()
+  }
+
+  giveScore(){
+    for(let p of players){
+      if(p.id != this.id & p.alive)p.score ++
+    }
+  }
+}
